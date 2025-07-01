c3fb4b26566db934d308f3517fcd34bde7b3f25a
Santiago <esparzacastillo6@gmail.com>
2025-07-01 15:44:24 -0500

    Ya cierra sesion y no nos devuelve a la ultima pantalla en la que estaba

--- app/src/main/java/com/market/inventarioapp/InicioActivity.kt
+++ app/src/main/java/com/market/inventarioapp/InicioActivity.kt
@@ -52,7 +52,9 @@ class InicioActivity : AppCompatActivity() {
 
         cardSalir.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
-            startActivity(Intent(this, LoginActivity::class.java))
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
             finish()
         }
 
